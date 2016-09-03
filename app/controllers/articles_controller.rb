4e1af1ec362cdfab9c6306db307bff0c0d2a0939
diegoversiani <diegoversiani@gmail.com>
2016-09-03 12:13:43 -0300

    Add delete articles action.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,10 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
     @article = Article.new
   end
@@ -34,8 +38,14 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def show
+  def destroy
     @article = Article.find(params[:id])
+    
+    if @article.destroy
+      flash[:notice] = "Article was successfully deleted"
+    end
+
+    redirect_to articles_path
   end
 
   private
