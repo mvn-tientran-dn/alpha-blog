0be648a6b0ddbd13aeb8f46b89de81bc31378604
diegoversiani <diegoversiani@gmail.com>
2016-09-03 11:40:12 -0300

    Add edit articles actions.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(articles_params)
     
@@ -11,8 +15,18 @@ class ArticlesController < ApplicationController
       flash[:notice] = "Article was successfully created"
       redirect_to article_path(@article)
     else
-
       render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    
+    if @article.update(articles_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
     end
   end
 
