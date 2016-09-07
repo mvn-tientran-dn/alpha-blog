4fedc8d93e9e5e172d1db26b8651c3f5d22aac6c
diegoversiani <diegoversiani@gmail.com>
2016-09-07 17:11:41 -0300

    Add admin property to users and update articles controller and views to allow admin users to perform update and delete action.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+admin_user = User.create(username: 'admin', email: 'admin@admin.com', admin: true)
