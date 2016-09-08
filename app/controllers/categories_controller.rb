410d0e309cdae95717f048193332f0239458add0
diegoversiani <diegoversiani@gmail.com>
2016-09-08 16:57:08 -0300

    Update views to include section-action in a div.row

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show]
   before_action :require_admin, except: [:index, :show]
 
   def index
@@ -21,12 +22,16 @@ class CategoriesController < ApplicationController
   end
 
   def show
-    
+    @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
 
   private
     def category_params
       params.require(:category).permit(:name)
+    end
+
+    def set_category
+      @category = Category.find(params[:id])
     end
 
     def require_admin
